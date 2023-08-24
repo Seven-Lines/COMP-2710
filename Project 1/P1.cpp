4012bb47a577d0b67a8d0462d04d2e9593df06bd
Sean <72239812+Seven-Lines@users.noreply.github.com>
2023-08-24 12:45:49 -0500

    Started Project 1
    Please commit suicide at earliest convenience :thumbsup:

---
+++ Project 1/P1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std; //<- using names from the standard library
+
+int main() {
+  cout << "Hello World!\n";
+  return 0;
+}
+
+// compile this shit first with the "g++" thing 
