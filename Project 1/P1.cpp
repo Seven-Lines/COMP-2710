d68903ed9551b20696b524d01ce35deff6040694
Sean <72239812+Seven-Lines@users.noreply.github.com>
2023-08-28 14:00:02 -0500

    suffering. pain, you could even say

--- Project 1/P1.cpp
+++ Project 1/P1.cpp
@@ -1,9 +1,25 @@
-#include <iostream> 
-using namespace std; //<- using names from the standard library
+/* FILE NAME - P1 ... for "Project 1"
+* AUTHOR: Sean Bevensee 
+* See Syllabus? Idk. 
+*
+* SEE "Project1.PDF" ON CANVAS
+*/
+
+#include <iostream>
+using namespace std;
 
 int main() {
-  cout << "Hello World!\n";
-  return 0;
+  int loan_amount, interest_rate, monthly_payments; 
+
+  cout.setf(ios::fixed);  // (currency formatting, idk.)
+  cout.setf(ios::showpoint);
+  cout.precision(2);
+
+  do { cout << "\nLoan Amount: "; cin >> loan_amount; } while(loan_amount < 0);
+  do { cout << "\nInterest Rate: "; cin >> interest_rate; } while (interest_rate < 0);
+  do { cout << "\nMonthly Payments: "; cin >> monthly_payments; } while ();
+
+
 }
 
-// compile this shit first with the "g++" thing 
+// compile this shit first with the "g++ P1.cpp" 
