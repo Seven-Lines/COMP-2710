8de5d020bc5a0160536119d4574f6f3b72557b87
Sean <72239812+Seven-Lines@users.noreply.github.com>
2023-08-29 13:22:47 -0500

    I think that's good
    cash money

--- Project 1/P1.cpp
+++ Project 1/P1.cpp
@@ -8,17 +8,32 @@
 #include <iostream>
 using namespace std;
 
+//----------- INITIALIZE VARIABLES -----------//
+float loan_amount, monthly_payments, interest_rate, interest_total; 
+int current_month = 0; 
+
+// Print Statment Function 
+void print(bool na, float principle_payment, float interest_payment) {
+  cout << current_month++ << "\t$" << loan_amount;
+  if (loan_amount < 1000) cout << "\t"; 
+  if (na) { 
+    cout << "  N/A\t\tN/A\tN/A\t\tN/A\n";
+  } else { 
+    cout << "  $" << monthly_payments << "\t" << interest_rate * 100 << "\t$" << interest_payment << "\t\t$" << principle_payment << "\n"; 
+  }
+}
+
+//----------- MAIN -----------//
 int main() {
-  //----------- INITIALIZE VARIABLES -----------//
-  int loan_amount, interest_rate, monthly_payments; 
+  // Formating #s...
+  cout.setf(ios::fixed);     // <- display decimal point and all trailing zeros
+  cout.setf(ios::showpoint); // <- shows decimal point
+  cout.precision(2);         // <- exactly 2 decimal points
 
-  cout.setf(ios::fixed);  // (currency formatting, idk.)
-  cout.setf(ios::showpoint);
-  cout.precision(2);
-
+  // Getting user input...
   do { cout << "\nLoan Amount: "; cin >> loan_amount; } while(loan_amount < 0);
-  do { cout << "Interest Rate: "; cin >> interest_rate; } while (interest_rate < 0);
-  do { cout << "Monthly Payments: "; cin >> monthly_payments; } while (monthly_payments <= 0 || monthly_payments <= loan_amount * interest_rate); 
+  do { cout << "Interest Rate: "; cin >> interest_rate; interest_rate = interest_rate / 12 / 100; } while (interest_rate < 0);
+  do { cout << "Monthly Payments: "; cin >> monthly_payments; } while (monthly_payments <= 0 || monthly_payments <= loan_amount * interest_rate);   
 
   //----------- PRINT OUTPUT -----------//
   cout << "*****************************************************************\n"
@@ -26,18 +41,27 @@ int main() {
   << "*****************************************************************\n"
   << "Month\tBalance\t\tPayment\tRate\tInterest\tPrincipal\n";
 
-  int current_month = 0; 
-  float interest_rate = 0;
-
+  // Main loop ... 
   while (loan_amount > 0){ 
-    if(current_month == 0) {
-      cout << current_month++ << "\t$" << loan_amount;  
-    }
+    // Exception handling ... 
+    if(current_month == 0) { 
+      print(true, 0, 0);
+    } else { 
+      float interest_payment = loan_amount * interest_rate; 
+      float principle_payment = monthly_payments - interest_payment;
 
-    //if(loan_amount < 1000) cout << "\t";
+      loan_amount -= principle_payment;
+      interest_total += interest_payment; 
 
-    
+      if (loan_amount < 0) { principle_payment += loan_amount; monthly_payments += loan_amount; loan_amount = 0; }
+      print(false, principle_payment, interest_payment); 
+    }    
   }
-}
+  cout << "****************************************************************\n";
+	cout << "\nIt takes " << --current_month << " months to pay off "
+	     << "the loan.\n"
+	     << "Total interest paid is: $" << interest_total;
+	cout << endl << endl;
 
-// compile this shit first with the "g++ P1.cpp" 
+  return 0; 
+}
