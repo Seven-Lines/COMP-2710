6ef87a54ffa80c5a18a0b29e5becedf9a1a20a68
Sean <72239812+Seven-Lines@users.noreply.github.com>
2023-08-29 12:27:09 -0500

    maybe consider suicide? idk. that's just how i'm feeling

--- Project 1/P1.cpp
+++ Project 1/P1.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 int main() {
+  //----------- INITIALIZE VARIABLES -----------//
   int loan_amount, interest_rate, monthly_payments; 
 
   cout.setf(ios::fixed);  // (currency formatting, idk.)
@@ -16,10 +17,27 @@ int main() {
   cout.precision(2);
 
   do { cout << "\nLoan Amount: "; cin >> loan_amount; } while(loan_amount < 0);
-  do { cout << "\nInterest Rate: "; cin >> interest_rate; } while (interest_rate < 0);
-  do { cout << "\nMonthly Payments: "; cin >> monthly_payments; } while ();
+  do { cout << "Interest Rate: "; cin >> interest_rate; } while (interest_rate < 0);
+  do { cout << "Monthly Payments: "; cin >> monthly_payments; } while (monthly_payments <= 0 || monthly_payments <= loan_amount * interest_rate); 
 
+  //----------- PRINT OUTPUT -----------//
+  cout << "*****************************************************************\n"
+  << "\tAmortization Table\n"
+  << "*****************************************************************\n"
+  << "Month\tBalance\t\tPayment\tRate\tInterest\tPrincipal\n";
 
+  int current_month = 0; 
+  float interest_rate = 0;
+
+  while (loan_amount > 0){ 
+    if(current_month == 0) {
+      cout << current_month++ << "\t$" << loan_amount;  
+    }
+
+    //if(loan_amount < 1000) cout << "\t";
+
+    
+  }
 }
 
 // compile this shit first with the "g++ P1.cpp" 
