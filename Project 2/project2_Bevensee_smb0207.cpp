8623a15084bec161f83a4e6477af6f3f717ed786
Sean <72239812+Seven-Lines@users.noreply.github.com>
2023-09-12 12:24:20 -0500

    Starting project 2

---
+++ Project 2/project2_Bevensee_smb0207.cpp
@@ -0,0 +1,45 @@
+/* 
+* AUTHOR: Sean Bevensee
+* AUB ID: smb0207
+* FILE NAME: project2_bevensee_smb0207.cpp
+*
+* To compile use command "g++ project1_Bevensee_smb0207.cpp"  
+* in parent directory. 
+*
+* I used the project file. [ n o t e s ]
+*/
+
+#include <iostream>
+using namespace std;
+
+//----------- INITIALIZE VARIABLES -----------//
+
+//----------- FUNCTIONS -----------//
+// At least two alive...
+bool at_least_two_alive(bool A_alive, bool B_alive, bool C_alive) { 
+    return false;
+}
+
+// Shoots... 
+void Aaron_shoots1(bool& B_alive, bool& C_alive) { 
+
+}
+
+void Aaron_shoots2(bool& B_alive, bool& C_alive) { 
+
+}
+
+void Bob_shoots(bool& A_alive, bool& C_alive) { 
+
+}
+
+void Charlie_shoots(bool& A_alive, bool& B_alive) { 
+
+}
+
+
+
+
+//----------- MAIN -----------//
+int main() { 
+    cout << "kys";
