6f3d8e4093b7a8d2aec9625025f95e6161c15c3d
Sean <sbevensee777@gmail.com>
2023-11-13 17:57:24 -0600

    Added project files
    im too lazy to do this shit at the moment

---
+++ Project 5/project5_Bevensee_smb0207.cpp
@@ -0,0 +1,11 @@
+/* 
+* AUTHOR: Sean Bevensee
+* AUB ID: smb0207
+* FILE NAME: project5_bevensee_smb0207.cpp
+*
+* To compile use command "g++ project5_Bevensee_smb0207.cpp"  in parent 
+* directory. Then execute the new executable file. It should be called 
+* "a.out".  
+*
+* I used the "Project5.pdf" file on Canvas.
+*/
